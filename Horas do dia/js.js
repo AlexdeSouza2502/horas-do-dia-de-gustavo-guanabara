da94464a3bd783f10aadbc6dd87283410a683886
Alex_Souza2002 <62069087+AlexdeSouza2502@users.noreply.github.com>
2023-02-23 20:20:34 -0300

    Add files via upload

--- Horas do dia/js.js
+++ Horas do dia/js.js
@@ -8,15 +8,15 @@ msg.innerHTML = `Agora são ${hora} horas.`
 if (hora >= 0 && hora < 12 ) {
  //Bom Dia
   img.src = 'manha.jpg'
-  document.body.style.background = '#FAC9C6'
+  document.body.style.backgroundImage = 'url(../manha.jpg)'
     //BOA Tarde  
 } else if(hora >= 12 && hora <= 18) {
     //Boa Tarde
     img.src = 'tarde.png'
-    document.body.style.background = '#EAC07F'
+    document.body.style.background = 'url(../tarde.png)'
 } else {
     //Boa Noite
     img.src = 'noite.jpg'
-    document.body.style.backgrou = '#074D88'
+    document.body.style.backgrou = 'url(../noite.jpg)'
   }
 }
